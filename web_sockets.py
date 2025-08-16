1e54e054e1b1f3beeb97ea35f785dc1cce82d863
Marikanti Srikar <msrikar@modeln.com>
2025-08-16 20:39:56 +0530

    Sockets start

---
+++ web_sockets.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, WebSocket
+import uvicorn
+
+app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Echo: {data}")
+
+if __name__ == "__main__":
+    uvicorn.run("web_sockets:app", host="0.0.0.0", port=8000, reload=True)
